9eafef2221fede10efba9e0151ffa908cf06fe32
Nick Lawson <niklasl.se@gmail.com>
2023-07-11 10:57:54 +0200

    Minor changes

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
+from flask import Flask, Response, request, jsonify
 import os
-import http.server
-import socketserver
 
-from http import HTTPStatus
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return Response('Hello? Yes, this is dog!', status=200)
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello? Yes, this is dog!'
-        self.wfile.write(msg.encode())
+@app.route('/api', methods=['POST'])
+def api():
+    # content = request.json
+    # Do something with the json content
+    # For demonstration purposes, let's just send it back as the response
+    # return jsonify(content)
+    return Response('/API', status=200)
 
-
-port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 80))
+    print('Listening on port %s' % (port))
+    app.run(host='0.0.0.0', port=port)
